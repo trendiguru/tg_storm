51d35eebd464fe31033d2a745f0b4c7e96eec3ff
Nadav <nadav@trendiguru.com>
2016-02-14 16:53:14 +0200

    testing storm pipeline

--- src/bolts/image.py
+++ src/bolts/image.py
@@ -85,8 +85,8 @@ class MergePeople(Bolt):
                                                                              image_id,
                                                                              self.bucket[image_id]['image_obj']['num_of_people']))
             if self.bucket[image_id]['person_stack'] == self.bucket[image_id]['image_obj']['num_of_people']:
-                # insert_result = db.images.insert_one(self.bucket[image_id]['image_obj'])
-                self.log("Done! all people for image {0} arrived, ready to Insert! :)".format(image_id))
-                # del self.bucket[image_id]
-                # if not insert_result.acknowledged:
-                #     self.log("Insert failed")
+                insert_result = db.images.insert_one(self.bucket[image_id]['image_obj'])
+                self.log("Done! all people for image {0} arrived, Inserting! :)".format(image_id))
+                del self.bucket[image_id]
+                if not insert_result.acknowledged:
+                    self.log("Insert failed")
