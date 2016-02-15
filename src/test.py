2dd124abafe71e25a1f6dc9d6a063094fa556814
Nadav <nadav@trendiguru.com>
2016-02-15 15:02:21 +0200

    testing storm pipeline

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ __author__ = 'Nadav Paz'
 from rq import push_connection, Queue
 from trendi.constants import redis_conn
 page_url = 'http://www.vogue.com/13396259/super-bowl-50-performers-choose-italian-brands/'
-image_url = 'http://cinefog.com/wp-content/uploads/2015/08/Skirt-Fashion-Trends-Fall-Winter-2015-2016-8.jpg'
+image_url = 'http://ell.h-cdn.co/assets/cm/15/02/980x490/54ac3a68ad2b5_-_elle-00-rainbow-opener-h-elh.jpg'
 push_connection(redis_conn)
 storm_pipeline = Queue('storm_pipeline')
 
