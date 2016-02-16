4a45c002d534c0616d2f099516bd34ed2592e3a1
Nadav <nadav@trendiguru.com>
2016-02-16 19:09:31 +0200

    testing storm pipeline

--- src/bolts/item.py
+++ src/bolts/item.py
@@ -12,28 +12,28 @@ class ItemBolt(Bolt):
     def initialize(self, conf, ctx):
         self.db = constants.db
 
-    def process(self, tup):
-        person_id = tup.values[0].pop('person_id')
-        item = tup.values[0]
-        item['mask'] = np.array(item['mask'], dtype=np.uint8)
-        item['image'] = np.array(item['image'], dtype=np.uint8)
-        out_item = {}
-        start = time.time()
-        out_item['fp'], out_item['similar_results'] = find_similar_mongo.find_top_n_results(item['image'],
-                                                                                            item['mask'],
-                                                                                            100, item['category'],
-                                                                                            'products')
-        self.log("back from find_top_n after {0} secs..".format(time.time() - start))
-        out_item['category'] = item['category']
-        self.emit([out_item, person_id])
-
-
     # def process(self, tup):
-    #     item, person_id = tup.values
+    #     person_id = tup.values[0].pop('person_id')
+    #     item = tup.values[0]
+    #     item['mask'] = np.array(item['mask'], dtype=np.uint8)
+    #     item['image'] = np.array(item['image'], dtype=np.uint8)
     #     out_item = {}
     #     start = time.time()
-    #     out_item['fp'], out_item['similar_results'] = 1, 2
-    #     time.sleep(5)
+    #     out_item['fp'], out_item['similar_results'] = find_similar_mongo.find_top_n_results(item['image'],
+    #                                                                                         item['mask'],
+    #                                                                                         100, item['category'],
+    #                                                                                         'products')
     #     self.log("back from find_top_n after {0} secs..".format(time.time() - start))
     #     out_item['category'] = item['category']
     #     self.emit([out_item, person_id])
+
+
+    def process(self, tup):
+        item, person_id = tup.values
+        out_item = {}
+        start = time.time()
+        out_item['fp'], out_item['similar_results'] = 1, 2
+        time.sleep(5)
+        self.log("back from find_top_n after {0} secs..".format(time.time() - start))
+        out_item['category'] = item['category']
+        self.emit([out_item, person_id])
