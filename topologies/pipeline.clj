5e9d708e6eff40680f2bd237112c5f36ec60a53c
Nadav <nadav@trendiguru.com>
2016-02-18 12:32:56 +0200

    testing storm pipeline

--- topologies/pipeline.clj
+++ topologies/pipeline.clj
@@ -2,7 +2,8 @@
   (:use     [streamparse.specs])
   (:gen-class))
 
-(defn pipeline [options]
+(defn pipeline [topology.enable.message.timeouts=true
+                topology.message.timeout.secs=1000]
    [
     ;; spout configuration
     {"image-spout" (python-spout-spec
