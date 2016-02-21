5edcee89de1ec08f242e2ef5b17321248839b2a2
Nadav <nadav@trendiguru.com>
2016-02-21 15:20:52 +0200

    testing storm pipeline

--- src/bolts/person.py
+++ src/bolts/person.py
@@ -69,7 +69,7 @@ class PersonBolt(Bolt):
         person['items'] = []
         self.log("sending to Herr paperdoll")
         start = time.time()
-        time.sleep(50)
+        time.sleep(15)
         self.log("back from paperdoll after {0} seconds..".format(time.time() - start))
         idx = 0
         items = []
