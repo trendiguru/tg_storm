396d06564e2a4dddd0bd4d643ef437f7c50b5570
Nadav <nadav@trendiguru.com>
2016-02-29 12:19:23 +0200

    whitelist -> txt file

--- src/spouts/new_images.py
+++ src/spouts/new_images.py
@@ -16,7 +16,6 @@ class NewImageSpout(Spout):
         if not job:
             return
         page_url, image_url, lang = job.args  # TODO - cancel the lang..
-        self.log("spouts page_url: {0}, image_url: {1}".format(page_url, image_url))
         self.emit([page_url, image_url], tup_id=image_url)
 
     def fail(self, tup_id):
