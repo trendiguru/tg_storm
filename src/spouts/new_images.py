0945dd8190f7051b994f63605df4dd741e587189
Nadav <nadav@trendiguru.com>
2016-04-05 11:22:15 +0300

    notifications update

--- src/spouts/new_images.py
+++ src/spouts/new_images.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from streamparse.spout import Spout
 import rq
-from trendi.constants import redis_conn
+from trendi.constants import redis_conn, db
 
 
 class NewImageSpout(Spout):
@@ -10,6 +10,7 @@ class NewImageSpout(Spout):
     def initialize(self, stormconf, context):
         rq.push_connection(redis_conn)
         self.q = rq.Queue("start_pipeline")
+        self.db = db
 
     def next_tuple(self):
         job = self.q.dequeue()
@@ -19,7 +20,9 @@ class NewImageSpout(Spout):
         self.emit([page_url, image_url], tup_id=image_url)
 
     def fail(self, tup_id):
-        self.log("OMG {0} FAILED".format(tup_id))
+        deleted = db.iip.delete_one({'image_url': tup_id}).deleted_count
+        var = deleted * 'and have' + (1-deleted) * 'but have not'
+        self.log("OMG {0} FAILED {1} been deleted from db.iip".format(tup_id, var))
 
     def ack(self, tup_id):
         self.log("HEYYY!.. {0} ACKNOWLEGED".format(tup_id))
