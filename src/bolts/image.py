a33d5ce61c125589ff35644fa40ca2c259569466
Nadav <nadav@trendiguru.com>
2016-05-04 17:14:33 +0300

    big_change

--- src/bolts/image.py
+++ src/bolts/image.py
@@ -24,15 +24,7 @@ class NewImageBolt(Bolt):
     def process(self, tup):
         page_url, image_url = tup.values
 
-        # check if image is in-process by url
-        tempo = db.iip.find_one({'image_url': image_url})
-        if tempo:
-            return
-
-        # check if page domain is in our white-list
         domain = tldextract.extract(page_url).registered_domain
-        if not db.whitelist.find_one({'domain': domain}):
-            return
 
         if image_url[:4] == "data":
             image = Utils.data_url_to_cv2_img(image_url)
@@ -41,14 +33,6 @@ class NewImageBolt(Bolt):
                 self.log("'data_url_to_cv2_img' has failed. Bad image!")
                 return
         else:
-            images_by_url = db.images.find_one({"image_urls": image_url})
-            if images_by_url:
-                return
-
-            images_obj_url = db.irrelevant_images.find_one({"image_urls": image_url})
-            if images_obj_url:
-                return
-
             image = Utils.get_cv2_img_array(image_url)
             if image is None:
                 self.log("'get_cv2_img_array' has failed. Bad image!")
@@ -60,64 +44,44 @@ class NewImageBolt(Bolt):
         if images_obj_hash:
             return
 
-        # get faces and relevancy
-        relevance = background_removal.image_is_relevant(image, use_caffe=False, image_url=image_url)
-        image_dict = {'image_urls': [image_url], 'relevant': relevance.is_relevant, 'views': 1,
+        gender_obj = db.genderator.find_one({'image_url': image_url})
+        image_dict = {'image_urls': [image_url], 'relevant': True, 'views': 1,
                       'saved_date': str(datetime.datetime.utcnow()), 'image_hash': image_hash, 'page_urls': [page_url],
-                      'people': [], 'image_id': str(bson.ObjectId())}
-        if relevance.is_relevant:
-            db.iip.insert_one({'image_url': image_url, 'insert_time': datetime.datetime.utcnow()})
-            # There are faces
-            people = []
-            idx = 0
-            for face in relevance.faces:
-                x, y, w, h = face
-                isolated_image = background_removal.person_isolation(image, face)
-                person_bb = [int(round(max(0, x - 1.5 * w))), str(y), int(round(min(image.shape[1], x + 2.5 * w))),
-                             min(image.shape[0], 8 * h)]
-                # INSERT TO YONATAN'S COLLECTION
-                db.yonatan_gender.insert_one({'url': image_url, 'face': face.tolist(), 'status': 'fresh',
-                                             'person_id': str(bson.ObjectId())})
-                monitoring.email(self.yonatans, 'New image to genderize!', ['yonatanguy@gmail.com'])
-                if domain in manual_gender_domains:
-                    person_id = str(bson.ObjectId())
-                    if not db.genderator.count():
-                        monitoring.email(self.stats, 'New image to genderize!', ['nadav@trendiguru.com',
-                                                                                 'yonatanguy@gmail.com'])
-                                                                                 # 'lior@trendiguru.com',
-                                                                                 # 'kyle@trendiguru.com'])
-                    db.genderator.insert_one({'url': image_url, 'face': face.tolist(), 'status': 'fresh',
-                                             'person_id': person_id})
-                    pers = db.genderator.find_one({'person_id': person_id})
-                    while pers['status'] != 'done':
-                        time.sleep(2)
-                        self.log(pers)
-                        pers = db.genderator.find_one({'person_id': person_id})
-                    gender = pers['gender']
-                    db.genderator.delete_one({'person_id': person_id})
-                    if gender != "not_relevant":
-                        person_args = {'face': face.tolist(), 'person_bb': person_bb, 'image_id': image_dict['image_id'],
-                                       'image': isolated_image.tolist(), 'gender': gender, 'domain': domain}
-                        people.append(person_args)
-                        idx += 1
-                else:
-                    person_args = {'face': face.tolist(), 'person_bb': person_bb, 'image_id': image_dict['image_id'],
-                                   'image': isolated_image.tolist(), 'domain': domain}
-                    idx += 1
-                    people.append(person_args)
-            image_dict['num_of_people'] = idx
-            self.log("gonna emit {0} as image_id".format(image_dict['image_id']))
-            self.emit([image_dict, image_dict['image_id']], stream='image_obj')
-            self.log('gonna emit {idx} people from {id}'.format(idx=idx, id=image_dict['image_id']))
-            for person in people:
-                self.emit([person], stream='person_args')
-            if not idx:
-                db.irrelevant_images.insert_one(image_dict)
-                db.iip.delete_one({'image_url': image_url})
-                self.log('{url} stored as irrelevant, wrong face was found'.format(url=image_url))
-        else:
+                      'people': gender_obj['people'], 'image_id': str(bson.ObjectId())}
+        db.iip.insert_one({'image_url': image_url, 'insert_time': datetime.datetime.utcnow()})
+        idx = 0
+        people_to_emit = []
+        for person in image_dict['people']:
+            face = person['face']
+            x, y, w, h = face
+            isolated_image = background_removal.person_isolation(image, face)
+            person_bb = [int(round(max(0, x - 1.5 * w))), str(y), int(round(min(image.shape[1], x + 2.5 * w))),
+                         min(image.shape[0], 8 * h)]
+            # INSERT TO YONATAN'S COLLECTION
+            db.yonatan_gender.insert_one({'url': image_url, 'face': face.tolist(), 'status': 'fresh',
+                                         'person_id': str(bson.ObjectId())})
+            monitoring.email(self.yonatans, 'New image to genderize!', ['yonatanguy@gmail.com'])
+            if 'gender' in person.keys():
+                gender = person['gender']
+            else:
+                gender = 'Female'
+
+            if gender != "not_relevant":
+                person_args = {'face': face.tolist(), 'person_bb': person_bb, 'image_id': image_dict['image_id'],
+                               'image': isolated_image.tolist(), 'gender': gender, 'domain': domain}
+                people_to_emit.append(person_args)
+                idx += 1
+
+        db.genderator.delete_one({'image_url': image_url})
+        image_dict['num_of_people'] = idx
+        self.emit([image_dict, image_dict['image_id']], stream='image_obj')
+        self.log('gonna emit {idx} people from {id}'.format(idx=idx, id=image_dict['image_id']))
+        for person in people_to_emit:
+            self.emit([person], stream='person_args')
+        if not idx:
             db.irrelevant_images.insert_one(image_dict)
-            self.log('{url} stored as irrelevant'.format(url=image_url))
+            db.iip.delete_one({'image_url': image_url})
+            self.log('{url} stored as irrelevant, wrong face was found'.format(url=image_url))
 
 
 class MergePeople(Bolt):
