47f338aa2ca6a828d3d8010bd4d739e8e405440a
Nadav <nadav@trendiguru.com>
2016-06-30 20:34:35 +0300

    falcon pd go!!

--- src/spouts/new_images.py
+++ src/spouts/new_images.py
@@ -20,11 +20,11 @@ class NewImageSpout(Spout):
         self.emit([self.page_url, self.image_url], tup_id=self.image_url)
 
     def fail(self, tup_id):
-        # deleted = db.iip.delete_one({'image_url': tup_id}).deleted_count
-        # var = deleted * 'and have' + (1-deleted) * 'but have not'
-        # self.log("OMG {0} FAILED {1} been deleted from db.iip".format(tup_id, var))
-        self.q.enqueue_call(func="", args=(self.page_url, self.image_url, None), ttl=2000, result_ttl=2000, timeout=2000)
-        self.log("{0} failed and got re-enqueued !".format(tup_id))
+        deleted = db.iip.delete_one({'image_url': tup_id}).deleted_count
+        var = deleted * 'and have' + (1-deleted) * 'but have not'
+        self.log("OMG {0} FAILED {1} been deleted from db.iip".format(tup_id, var))
+        # self.q.enqueue_call(func="", args=(self.page_url, self.image_url, None), ttl=2000, result_ttl=2000, timeout=2000)
+        # self.log("{0} failed and got re-enqueued !".format(tup_id))
 
     def ack(self, tup_id):
         self.log("HEYYY!.. {0} ACKNOWLEGED".format(tup_id))
