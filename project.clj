2a6230b694ede5933e907cc2b33d34ef2f25f00e
Nadav <nadav@trendiguru.com>
2016-09-13 15:36:37 +0300

    org.apache.storm remove

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject wc "0.0.1-SNAPSHOT"
+(defproject pipeline "0.0.1-TG_STORM"
   :resource-paths ["_resources"]
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
   :dependencies  [[org.apache.storm/storm-core "1.0.1"]
                   [org.apache.storm/flux-core "1.0.1"]]
-  :jar-exclusions     [#"log4j\.properties" #"org\.apache\.storm\.(?!flux)" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
-  :uberjar-exclusions [#"log4j\.properties" #"org\.apache\.storm\.(?!flux)" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  :jar-exclusions     [#"log4j\.properties" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  :uberjar-exclusions [#"log4j\.properties" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
