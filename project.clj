de86c4823d3c4e8f8b0289d75b544d4969f06906
Nadav <nadav@trendiguru.com>
2016-09-13 16:05:58 +0300

    image_urls

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject pipeline "0.0.1-TG_STORM"
+(defproject pipeline "0.0.1-SNAPSHOT"
+  :source-paths ["topologies"]
   :resource-paths ["_resources"]
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
   :dependencies  [[org.apache.storm/storm-core "1.0.1"]
-                  [org.apache.storm/flux-core "1.0.1"]]
-  :jar-exclusions [#"log4j.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #".yaml"]
-  :uberjar-exclusions [#"log4j.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #".yaml"]
-  )
+                  [com.parsely/streamparse "0.0.4-SNAPSHOT"]
+                  ]
+  :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
