9dd16adb6c90edcbbbad81c5a03f16f0db59114e
Nadav <nadav@trendiguru.com>
2016-09-21 20:02:23 +0300

    only db.iip

--- src/bolts/image.py
+++ src/bolts/image.py
@@ -32,10 +32,10 @@ class NewImageBolt(Bolt):
 
         image_hash = page_results.get_hash(image)
 
-        gender_obj = db.genderator.find_one({'image_url': image_url})
+        temp_obj = db.iip.find_one({'image_urls': image_url})
         image_dict = {'image_urls': [image_url], 'relevant': True, 'views': 1,
                       'saved_date': str(datetime.datetime.utcnow()), 'image_hash': image_hash, 'page_urls': [page_url],
-                      'people': gender_obj['people'], 'image_id': str(bson.ObjectId()), 'domain': domain}
+                      'people': temp_obj['people'], 'image_id': str(bson.ObjectId()), 'domain': domain}
         idx = 0
         people_to_emit = []
         for person in image_dict['people']:
