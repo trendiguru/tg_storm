b5aecb14373892b6e8827916c61f91612cdbfeaa
Nadav <nadav@trendiguru.com>
2016-09-27 11:32:26 +0300

    storm stuck

--- src/spouts/new_images.py
+++ src/spouts/new_images.py
@@ -13,11 +13,13 @@ class NewImageSpout(Spout):
         self.db = db
 
     def next_tuple(self):
+        self.log("BEFORE DEQUEING")
         job = self.q.dequeue()
-        if not job:
-            return
+        self.log("AFTER DEQUEING")
         self.page_url, self.image_url, products, method = job.args
+        self.log("BEFORE EMITING")
         self.emit([self.page_url, self.image_url, products, method], tup_id=self.image_url)
+        self.log("AFTER DEQUEING")
 
     def fail(self, tup_id):
         deleted = db.iip.delete_one({'image_urls': tup_id}).deleted_count
