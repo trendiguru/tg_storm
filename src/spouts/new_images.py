48775021942189954b165a09aba996f984ee26c2
Nadav <nadav@trendiguru.com>
2016-09-27 14:49:34 +0300

    storm stuck

--- src/spouts/new_images.py
+++ src/spouts/new_images.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from streamparse.spout import Spout
-import rq, time
+import rq
 from trendi.constants import redis_conn, db
 
 
