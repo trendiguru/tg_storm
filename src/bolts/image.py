67806ee9ad7f3683dba510d687cb05dfcd114703
Nadav <nadav@trendiguru.com>
2016-10-20 14:45:51 +0300

    dups

--- src/bolts/image.py
+++ src/bolts/image.py
@@ -79,7 +79,8 @@ class MergePeople(Bolt):
             if self.bucket[image_id]['person_stack'] == self.bucket[image_id]['image_obj']['num_of_people']:
                 image_obj = self.bucket[image_id]['image_obj']
                 image_obj['saved_date'] = datetime.datetime.strptime(image_obj['saved_date'], "%Y-%m-%d %H:%M:%S.%f")
-                db.images.find_one_and_replace({'image_urls': image_obj['image_urls'][0]}, image_obj, upsert=True)
+                if not db.images.find_one({'image_urls': image_obj['image_urls'][0]}):
+                    db.images.insert_one(image_obj)
                 db.iip.delete_one({'image_urls': image_obj['image_urls'][0]})
                 self.log("Done! all people for image {0} arrived, Inserting! :)".format(image_id))
                 del self.bucket[image_id]
